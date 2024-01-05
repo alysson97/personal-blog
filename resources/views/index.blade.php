78ec9fb468d923dd507129f2196dbe6b0d39d2bc
alysson97 <alysson18.af@gmail.com>
2024-01-05 10:04:08 -0300

    hello world

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('header.header')
+
+@section('title', 'blog')
+
+
+
+@section('body')
+<main class="vh-100 d-flex justify-content-center align-items-center" >
+  <h1>Hello World!</h1>
+</main>
+
+@stop
