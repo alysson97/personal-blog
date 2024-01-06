e23adfdf9bf2fe6cfd28969d8f8ccbd1905d82fb
alysson97 <alysson18.af@gmail.com>
2024-01-06 05:00:00 -0300

    preview layout

--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -8,7 +8,16 @@
   
   <title>@yield('title')</title>
 </head>
-<body>
+<body class="vh-100">
+  <header class="vw-100 d-flex justify-content-between align-items-center py-2">
+    <div class="home">
+      <div class="logo"></div>
+    </div>
+    <div class="account col-2">
+      <button class="btn btn-light">Sign-in</button>
+      <button class="btn btn-light">Sign-up</button>
+    </div>
+  </header>
   @yield('body')
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,53 @@
 
 
 @section('body')
-<main class="vh-100 d-flex justify-content-center align-items-center" >
-  <h1>Hello World!</h1>
+<main class="row" >
+  <div class="comunitys col-2">
+    <table class="table table-hover">
+      <tr class="d-flex flex-column ">
+        <td>Mad-Devs</td>
+        <td>Disclaimers</td>
+        <td>Incels</td>
+        <td>Boomers</td>
+      </tr>
+    </table>
+  </div>
+
+  <!--Posts-->
+  <div class="post-wall col-8 container d-flex flex-column justify-content-center align-items-center">
+    <div class="row col-8 py-4">
+      <textarea name="Post" id="" cols="5" rows="5" placeholder="Write here..."></textarea>
+      <input type="file" name="image" id="">
+      <button class="col-2">Post</button>
+    </div>
+    <div class="card row col-8 p-3" style="background-color: #ffffff2a">
+      <div class="row d-flex justify-between">
+        <img src="desorganizado.png" alt="" class="border-radius col-2" style="">
+        <h3 class="col-10">Title</h3>
+      </div>
+      <div class="row">
+        <p class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ullam quibusdam incidunt maiores quo placeat aspernatur reprehenderit perferendis! Ratione consequatur sapiente obcaecati et magnam asperiores tempore exercitationem aliquam odit nostrum.
+        </p>
+      </div>
+      <div class="row">
+        <p>comments</p>
+        <button>like</button> <button>dislike</button>
+      </div>
+    </div>
+  </div>
+  
+
+  <div class="friends col-2">
+    <table class="table table-hover">
+      <tr class="d-flex flex-column ">
+        <td>Lara</td>
+        <td>Lua</td>
+        <td>Math</td>
+        <td>Tai</td>
+      </th>
+    </table>
+  </div>
 </main>
 
 @stop
